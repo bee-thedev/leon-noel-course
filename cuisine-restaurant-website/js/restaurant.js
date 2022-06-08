3c90cd995f387c2554a590e872c6524dbe75c286
bee-thedev <beenyshsaeed@gmail.com>
2022-06-08 14:12:44 +0500

    Section callus added

--- cuisine-restaurant-website/js/restaurant.js
+++ cuisine-restaurant-website/js/restaurant.js
@@ -226,3 +226,43 @@ document.addEventListener('keyup',function(e){
 });
 
 loadGallery();
+
+
+// ----------------------- Back on Top Functionality ----------------------------
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// ------------Blink Phone Number in Callus Section -----------
+
+		var blink =
+			document.getElementById('blink');
+
+		setInterval(function () {
+			blink.style.opacity =
+			(blink.style.opacity == 0 ? 1 : 0);
+		}, 500);
+
